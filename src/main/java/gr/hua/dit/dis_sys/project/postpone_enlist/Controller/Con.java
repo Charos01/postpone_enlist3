426f6d32b204a0a6c22217760dfd153427787637
Charos01 <80354208+Charos01@users.noreply.github.com>
2022-06-20 04:32:07 +0300

    Add files via upload

---
+++ src/main/java/gr/hua/dit/dis_sys/project/postpone_enlist/Controller/Con.java
@@ -0,0 +1,33 @@
+package gr.hua.dit.dis_sys.project.postpone_enlist.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class MailSubmissionController {
+
+    private final JavaMailSender javaMailSender;
+
+    @Autowired
+    MailSubmissionController(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @RequestMapping("/mail")
+    @ResponseStatus(HttpStatus.CREATED)
+    SimpleMailMessage send(String mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail);
+        mailMessage.setReplyTo("charos@mailhog.gr");
+        mailMessage.setFrom("someone@localhost");
+        mailMessage.setSubject("Welcome message");
+        mailMessage.setText("Welcomee!!");
+        javaMailSender.send(mailMessage);
+        return mailMessage;
+    }
+}
